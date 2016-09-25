21368cdcc3ae0215d944c2f47db33cc8dfe93aff
slightair <arksutite@gmail.com>
2016-09-26 00:17:14 +0900

    Add nullability, dictionary type

--- Pod/Classes/PURLog.h
+++ Pod/Classes/PURLog.h
@@ -9,7 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) NSString *identifier;
 @property (nonatomic, readonly) NSString *tag;
 @property (nonatomic, readonly) NSDate *date;
-@property (nonatomic, readonly) NSDictionary *userInfo;
+@property (nonatomic, readonly, nullable) NSDictionary<NSString *, id> *userInfo;
 
 @end
 
