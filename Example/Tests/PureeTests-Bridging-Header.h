fa6f857f896706e18f1d43a382dc2205ae0a3a90
slightair <arksutite@gmail.com>
2016-10-02 21:28:43 +0900

    Remove unnecessary import

--- Example/Tests/PureeTests-Bridging-Header.h
+++ Example/Tests/PureeTests-Bridging-Header.h
@@ -2,6 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <CocoaLumberjack/DDLog.h>
-#import <CocoaLumberjack/DDTTYLogger.h>
 #import <Puree/Puree.h>
